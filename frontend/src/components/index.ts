977997330b0f490a0939b6de00cfccf8adcb172b
iam-dev <affiliaps@gmail.com>
2024-12-24 18:02:37 +0100

    add frontend files

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,8 @@
+export * from './icons';
+export * from './main-nav';
+export * from './site-header';
+export * from './tailwind-indicator';
+export * from './theme-provider';
+export * from './theme-toggle';
+
+export * from './ui';
